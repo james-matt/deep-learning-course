566b3030887ff341a2ac53f6a3dca4786574f183
james.taggart.43 <james.taggart.43@gmail.com>
2020-05-31 17:35:58 -0500

    ANN predictions on leaving bank.

--- Artificial_Neural_Networks/myann.py
+++ Artificial_Neural_Networks/myann.py
@@ -41,6 +41,27 @@ X_test = sc.transform(X_test)
 # Fitting classifier to the Training set
 # Create your classifier here
 
+# PART 2 - Let's make the ANN
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialize ANN
+classifier = Sequential()
+
+# Adding input layer and the first hidden layer
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_dim=11))
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
+
+# Add the output layer
+classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+
+#Compiling the ANN
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Fitting ANN to training set.
+classifier.fit(X_train, y_train, batch_size=10, epochs=100)
+
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
